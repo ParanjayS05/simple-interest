87e2a2517e3327ac5f431080ac11a26c268766e7
ParanjayS05 <senapatiparanjay03@gmail.com>
2026-07-08 13:16:02 +0530

    simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+# Formula: Simple Interest = (Principal * Rate * Time) / 100
+
+echo "Simple Interest Calculator"
+
+echo "Enter the principal amount:"
+read principal
+
+echo "Enter the rate of interest:"
+read rate
+
+echo "Enter the time period in years:"
+read time
+
+simple_interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+
+echo "The simple interest is: $simple_interest"
